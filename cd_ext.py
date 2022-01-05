604fca471592ba1ac8b6399fb4f5465abe09cda6
kvichans <kvichans@mail.ru>
2022-01-05 12:50:51 +0300

    fix

--- cd_ext.py
+++ cd_ext.py
@@ -3,7 +3,7 @@ Authors:
     Andrey Kvichansky   (kvichans on github.com)
     Alexey Torgashin    (CudaText)
 Version:
-    '1.7.35 2022-01-05'
+    '1.7.36 2022-01-05'
 ToDo: (see end of file)
 '''
 import  re, os, sys, json, time, traceback, unicodedata
